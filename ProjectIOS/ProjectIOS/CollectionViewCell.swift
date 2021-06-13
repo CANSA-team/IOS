c026ba26cc059b640bc6bacf2605a6cafaadbcf8
hoanganh100k <hoanganh34k@gmail.com>
2021-06-13 18:41:17 +0700

    Update CollectionViewCell.swift

--- ProjectIOS/ProjectIOS/CollectionViewCell.swift
+++ ProjectIOS/ProjectIOS/CollectionViewCell.swift
@@ -2,7 +2,8 @@
 //  CollectionViewCell.swift
 //  ProjectIOS
 //
-//  Created by intozoom on 5/28/21.
+//  Created by Hoàng Anh on 5/28/21.
+//  File này được sử dụng khi được các collectionView kế thừa để đổ dữ liệu vào
 //
 
 import UIKit
@@ -16,6 +17,7 @@ class CollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var checkResult: UIButton!
     
+    //Đây là hàm set kết quả cho màn hình Result
     func result(with str:String,with check:Int){
         checkResult.setTitle(str, for: .normal)
         if (check == 1){
@@ -26,24 +28,28 @@ class CollectionViewCell: UICollectionViewCell {
 
         }
     }
+    //Set nút button của màn hình main
     func configButton(with str : String,with img:String){
         btnContent.setTitle(str, for: .normal)
         btnContent.setImage(UIImage(named: img), for: .normal)
         btnContent.addTarget(self,action: #selector(didPressButton(sender:)), for: .touchUpInside)
         
     }
+    //Đây là hàm đổ câu trả lời
     func ConfigQuestion(with str:String) {
         LableQuestion.text = str
-        DataPassing.shared.allButtons.append(QuestionButton)
-        QuestionButton.addTarget(self,action: #selector(SellectButton(sender:)), for: .touchUpInside)
+        DataPassing.shared.allButtons.append(QuestionButton) //Đổ thêm nút button vào mảng Button
+        QuestionButton.addTarget(self,action: #selector(SellectButton(sender:)), for: .touchUpInside) //đây là sự kiện
     }
     
+    //Set Sự kiện cho mỗi button bên màn hình main
     @objc func didPressButton(sender: UIButton!) {
+        //Khi click vào thì sẽ gán cho DataPassing
         DataPassing.shared.loaiBang = sender.currentTitle
     }
     
     //check button
-    //Radio Button Event
+    //Set Sự kiện RadioButton cho những button câu trả lời
     @objc func SellectButton(sender: UIButton!) {
         if sender.isSelected{
             sender.isSelected = false
